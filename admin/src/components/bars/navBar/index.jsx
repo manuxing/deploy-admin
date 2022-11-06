dad85bfd86c0ad8be2dcb5e39277d7d8ff77f136
Pedro Romano <romanopedro25@gmail.com>
2022-11-06 13:47:59 -0300

    sacando barra de todos menos about

--- admin/src/components/bars/navBar/index.jsx
+++ admin/src/components/bars/navBar/index.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { getNot } from "../../../redux/actions";
+import { useHistory } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import SearchBar from "../searchBar/index";
 import { NavLink } from "react-router-dom";
@@ -9,19 +10,33 @@ import "../navBar/navbar.css";
 import Drop from "./drop";
 
 const NavBar = () => {
-  
+  let navigate = useHistory()
+  navigate = navigate.location.pathname;
   let dispatch = useDispatch();
   let not = useSelector((state) => state.not);
   let [l, setL] = useState("");
   let [pressed, setPressed] = useState(false);
+  let [search, setSearch] = useState(true);
+  let searchAdresses = [
+    "/activitys",
+    "/activitys/",
+    "/reviews/",
+    "/requests/",
+    "/services/",
+    "/clients/",
+  ]
 
   useEffect(() => {
     dispatch(getNot());
   }, [dispatch]);
 
   useEffect(() => {
-    console.log(pressed)
-  }, [pressed]);
+    if(searchAdresses.includes(navigate)){
+      setSearch(true)
+    }else{
+      setSearch(false)
+    }
+  }, [navigate]);
 
   useEffect(() => {
     if (not.length > 0 && l !==not.length) {
@@ -56,7 +71,10 @@ const NavBar = () => {
               <Drop not={not} l={l}/> : <></>
             }
           </div>
-            <SearchBar />
+            {search === true ?
+            <SearchBar /> :
+          <></>
+          }
         </div>
       </div>
   );
