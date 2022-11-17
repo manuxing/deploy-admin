df9dbc7475ba40cca60acb7def4e1db183559075
Pedro Romano <romanopedro25@gmail.com>
2022-11-17 14:39:54 -0300

    en

--- client/client/src/components/About/index.jsx
+++ client/client/src/components/About/index.jsx
@@ -9,7 +9,7 @@ const About = ()=> {
     let history = useHistory();
     useEffect(()=>{
         axios
-            .get(`about`)
+            .get(` ${process.env.REACT_APP_API_URL}about`)
             .then((res) =>  {
               let cont = JSON.stringify(res.data.contact);
               res.data.contact = JSON.parse(cont)
